7350eb1db72ed4493762fc0c5e243a1b311bfd8e
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-19 11:21:36 +0800

    @wzm

--- isArrayLike.js
+++ isArrayLike.js
@@ -1,7 +1,7 @@
 // @wzm 2022-12-5
 import isLength from './isLength.js'
 
-/**
+/**函数length表示形参个数
  * Checks if `value` is array-like. A value is considered array-like if it's
  * not a function and has a `value.length` that's an integer greater than or
  * equal to `0` and less than or equal to `Number.MAX_SAFE_INTEGER`.
